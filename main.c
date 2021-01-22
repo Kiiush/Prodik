a5f34d478a12d3f6d26b8cae1d057f25d007a77b
Kiiush <64624034+Kiiush@users.noreply.github.com>
2021-01-22 16:34:31 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///                  Projeto Sistema de Gestão Escolar                      ///
+///                Developed by  @flgorgonio - Jan, 2021                    ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void menuPrincipal(void);
+
+int main(void) {
+    menuPrincipal();
+    return 0;
+
+}
+
+
+void menuPrincipal(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Serido                     ///\n");
+    printf("///               Departamento de Computacao e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programacao                      ///\n");
+    printf("///                  Projeto Sistema de Gestao Escolar                      ///\n");
+    printf("///                Developed by   @cayodts   - Jan, 2021                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///           = = = = Programa de Digitacao do Kiiush = = = =               ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Prodik e um programa feito com o intuito de ajudar aqueles que querem  ///\n");
+    printf("///  digitar de forma mais rapida e pratica, com a ajuda de diversos jogos  ///\n");
+    printf("///  e desafios para treinar seus reflexos e habilidades de digitacao.      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Seja para treinar ou apenas se divertir, Prodik podera te ajudar       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
