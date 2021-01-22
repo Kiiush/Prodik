b1a6f36168f7b4a7b83690641a89c8604f499b3a
Kiiush <cayoamizade@gmail.com>
2021-01-22 16:13:13 -0300

    inicializando

---
+++ main.c
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///                  Projeto Sistema de Gestão Escolar                      ///
+///                Developed by  @flgorgonio - Jan, 2021                    ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void menuPrincipal(void);
+
+int main(void) {
+    menuPrincipal();
+    return 0;
+
+}
+
+
+void menuPrincipal(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///                  Projeto Sistema de Gestão Escolar                      ///\n");
+    printf("///                Developed by   @cayodts   - Jan, 2021                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///           = = = = Programa de Digitação do Kiiush = = = =               ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Prodik é um programa feito com o intuito de ajudar aqueles que querem  ///\n");
+    printf("///  digitar de forma mais rápida e prática, com a ajuda de diversos jogos  ///\n");
+    printf("///  e desafios para treinar seus reflexos e habilidades de digitação.      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Seja para treinar ou apenas se divertir, Prodik poderá te ajudar       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
