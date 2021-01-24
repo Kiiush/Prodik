e4bb69b9cc919542450a8049baa57b0a34a5bc8f
Kiiush <cayoamizade@gmail.com>
2021-01-24 13:30:44 -0300

    menuSobre atualizado e variavel do menuPrincipal criada

--- main.c
+++ main.c
@@ -9,28 +9,47 @@
 
 #include <stdio.h>
 
+void menuSobre(void);
 void menuPrincipal(void);
 
 int main(void) {
-    menuPrincipal();
+    menuSobre();
     return 0;
 
 }
 
-
-void menuPrincipal(void) {
+//esse vai ser o menu que vai aparecer quando o usuário clicar na opção "sobre nos" no menuPrincipal
+void menuSobre(void) {
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///           = = = = Programa de Digitacao do Kiiush = = = =               ///\n");
+    printf("///                          ***************                                ///\n");
+    printf("///                          ** SOBRE NOS **                                ///\n");
+    printf("///                          ***************                                ///\n");
     printf("///                                                                         ///\n");
     printf("///  Prodik e um programa feito com o intuito de ajudar aqueles que querem  ///\n");
     printf("///  digitar de forma mais rapida e pratica, com a ajuda de diversos jogos  ///\n");
     printf("///  e desafios para treinar seus reflexos e habilidades de digitacao.      ///\n");
-    printf("///                                                                         ///\n");
     printf("///  Seja para treinar ou apenas se divertir, Prodik podera te ajudar       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+//ainda estou trabalhando no menu principal então por enquanto vai ser só uma copia do menuSobre
+void menuPrincipal(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///                  *************************************                  ///\n");
+    printf("///                  ** Programa de Digitacao do Kiiush **                  ///\n");
+    printf("///                  *************************************                  ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Prodik e um programa feito com o intuito de ajudar aqueles que querem  ///\n");
+    printf("///  digitar de forma mais rapida e pratica, com a ajuda de diversos jogos  ///\n");
+    printf("///  e desafios para treinar seus reflexos e habilidades de digitacao.      ///\n");
+    printf("///  Seja para treinar ou apenas se divertir, Prodik podera te ajudar       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
 }
