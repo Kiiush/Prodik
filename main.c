cb2b196cbb6da67df14a1c04be5c694188adf3d8
Kiiush <cayoamizade@gmail.com>
2021-01-25 17:18:18 -0300

    Menu principal terminado

--- main.c
+++ main.c
@@ -1,19 +1,11 @@
-///////////////////////////////////////////////////////////////////////////////
-///             Universidade Federal do Rio Grande do Norte                 ///
-///                 Centro de Ensino Superior do Seridó                     ///
-///               Departamento de Computação e Tecnologia                   ///
-///                  Disciplina DCT1106 -- Programação                      ///
-///                  Projeto Sistema de Gestão Escolar                      ///
-///                Developed by  @flgorgonio - Jan, 2021                    ///
-///////////////////////////////////////////////////////////////////////////////
-
 #include <stdio.h>
 
 void menuSobre(void);
 void menuPrincipal(void);
 
 int main(void) {
-    menuSobre();
+	menuPrincipal();
+	menuSobre();
     return 0;
 
 }
@@ -23,33 +15,35 @@ void menuSobre(void) {
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///                          ***************                                ///\n");
-    printf("///                          ** SOBRE NOS **                                ///\n");
-    printf("///                          ***************                                ///\n");
+    printf("///                            ***************                              ///\n");
+    printf("///                            ** SOBRE NOS **                              ///\n");
+    printf("///                            ***************                              ///\n");
     printf("///                                                                         ///\n");
     printf("///  Prodik e um programa feito com o intuito de ajudar aqueles que querem  ///\n");
     printf("///  digitar de forma mais rapida e pratica, com a ajuda de diversos jogos  ///\n");
     printf("///  e desafios para treinar seus reflexos e habilidades de digitacao.      ///\n");
     printf("///  Seja para treinar ou apenas se divertir, Prodik podera te ajudar       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                     Pressione <ESC> para voltar                         ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
 }
 
-//ainda estou trabalhando no menu principal então por enquanto vai ser só uma copia do menuSobre
+//menu principal terminado
 void menuPrincipal(void) {
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///                  *************************************                  ///\n");
-    printf("///                  ** Programa de Digitacao do Kiiush **                  ///\n");
-    printf("///                  *************************************                  ///\n");
-    printf("///                                                                         ///\n");
-    printf("///  Prodik e um programa feito com o intuito de ajudar aqueles que querem  ///\n");
-    printf("///  digitar de forma mais rapida e pratica, com a ajuda de diversos jogos  ///\n");
-    printf("///  e desafios para treinar seus reflexos e habilidades de digitacao.      ///\n");
-    printf("///  Seja para treinar ou apenas se divertir, Prodik podera te ajudar       ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                               ///\n");
+    printf("///             *************************************             ///\n");
+    printf("///             ** Programa de Digitacao do Kiiush **             ///\n");
+    printf("///             *************************************             ///\n");
+    printf("///                                                               ///\n");
+    printf("///                       1 - Jogar                               ///\n");
+    printf("///                       2 - Configuracoes                       ///\n");
+    printf("///                       3 - Sobre Nos                           ///\n");
+    printf("///                       4 - Sair                                ///\n");
+    printf("///                                                               ///\n");
+    printf("/////////////////////////////////////////////////////////////////////\n");
     printf("\n");
 }
